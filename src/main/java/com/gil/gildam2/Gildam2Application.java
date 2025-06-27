238e6c470d32a8dba744acfe2ea1bcf4703870d8
junseong kim <-67666411+js-kim-arc@users.noreply.github.com>
2025-06-27 15:04:47 +0900

    🎉 init: Spring Boot 프로젝트 첫 커밋

---
+++ src/main/java/com/gil/gildam2/Gildam2Application.java
@@ -0,0 +1,13 @@
+package com.gil.gildam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gildam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gildam2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gil/gildam2/Gildam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gil.gildam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gildam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
